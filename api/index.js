4e7f92289e5aa5c3c977420c61b4a93b4f3d95d5
saeedehsp <s.sadeghpour97@gmail.com>
2018-08-25 13:00:35 +0430

    feat:csv in handled

---
+++ api/index.js
@@ -0,0 +1,22 @@
+'use strict';
+const express = require('express');
+const cors = require('cors')
+const app = module.exports = express();
+const bodyparser = require('body-parser');
+app.use(cors());
+
+app.listen(3001, function () {
+    console.log('listening on 3001')
+})
+
+app.use(bodyparser.json());
+
+const{ getfile , upload} = require('./lib/importer');
+
+app.post('/import', upload ,async function(req,res){
+    res.sendStatus(200);
+    
+    const array= await getfile(req.file );
+    console.log(array ,"must wait");
+      
+})
