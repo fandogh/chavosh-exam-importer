a97b2618958dc4419a02585690d69bedfa81e09b
unknown <zahra.nejadian@gmail.com>
2018-08-29 09:54:43 +0430

    function completed

--- api/lib/importer.js
+++ api/lib/importer.js
@@ -5,21 +5,23 @@ const path = require('path')
 const multer = require('multer')
 var moment = require('moment-jalaali')
 const im = require('../fields').exam
-const upload = multer({ // multer settings
+
+const upload = multer({ //multer settings
   dest: 'uploads/',
-  fileFilter: function (req, file, callback) { // file filter
+  fileFilter: function (req, file, callback) { //file filter
     if (['.xls', '.xlsx', '.csv'].indexOf(path.extname(file.originalname)) === -1) {
-      return callback(new Error('Wrong extension type'))
+      return callback(new Error('Wrong extension type'));
     }
-    callback(null, true)
+    callback(null, true);
   }
-}).single('file') // multer is used to add file in a directory
+}).single('file') //multer is used to add file in a directory
 
 var myExam = {}
 
 const field = {
   course_code: im.fields.course_code.title,
   course_name: im.fields.course_name.title,
+
   date: im.fields.date.title,
   level: im.fields.level.title,
   semester: im.fields.semester.title,
@@ -31,32 +33,35 @@ const field = {
     location: im.fields.location.title,
     prof_name: im.fields.prof_name.title,
     prof_family_name: im.fields.prof_family_name.title,
-    course_group: im.fields.course_group.title
+    course_group: im.fields.course_group.title,
+
   }
-} // filed is used to access the the name of the fields in excel file
+}; // filed is used to access the the name of the fields in excel file
 
-async function imported (jsonArray) {
-  var exam = {}
-  var participants = []
+async function imported(jsonArray) {
+  var exam = {};
+  var participants = [];
 
+  //loops are for reading row element with correct feild name
   for (var i = 0; i < jsonArray.length; i++) {
+
     participants.push({
-      std_name: content(i, jsonArray, field).stdname,
-      std_family_name: content(i, jsonArray, field).fname,
-      std_number: content(i, jsonArray, field).stdno,
-      seat: content(i, jsonArray, field).seatno,
-      location: content(i, jsonArray, field).vloc,
-      prof_name: content(i, jsonArray, field).pname,
-      prof_family_name: content(i, jsonArray, field).profname,
-      course_group: content(i, jsonArray, field).courseG
+      std_name: content(i, jsonArray, field).dynamics[0],
+      std_family_name: content(i, jsonArray, field).dynamics[1],
+      std_number: content(i, jsonArray, field).dynamics[2],
+      seat: content(i, jsonArray, field).dynamics[3],
+      location: content(i, jsonArray, field).dynamics[4],
+      prof_name: content(i, jsonArray, field).dynamics[5],
+      prof_family_name: content(i, jsonArray, field).dynamics[6],
+      course_group: content(i, jsonArray, field).dynamics[7]
     });
   }
   exam = {
-    course_code: content(2, jsonArray, field).vsem[0],
-    course_name: content(2, jsonArray, field).vsem[1],
+    course_code: content(2, jsonArray, field).statics[0],
+    course_name: content(2, jsonArray, field).statics[1],
     date: content(2, jsonArray, field).dateform,
-    level: content(2, jsonArray, field).vsem[2],
-    semester: content(2, jsonArray, field).vsem[3]
+    level: content(2, jsonArray, field).statics[2],
+    semester: content(2, jsonArray, field).statics[3]
   };
 
   return {
@@ -102,63 +107,21 @@ module.exports = {
 }
 
 function content(i, jsonArray, field) {
-    var fname = ''
-    var stdno = ''
-    var profname = ''
-    var seatno = ''
-    var dateform = ''
-    var stdname = ''
-    var courseG = ''
-    var pname = ''
-    var vsem = ''
-    var vloc = ''
+  var dateform = ''
+  var dynamics = []
+  var statics = []
   var x = 0
+  var y = 0
+
   for (var key in field)
-
-    if (key === "participant") {
-      for (let index = 0; index < field.participant.std_name.length; index++) {
-        if (jsonArray[i][field.participant.std_name[index]] != undefined) {
-          stdname = jsonArray[i][field.participant.std_name[index]];
+    if (key === "participant")
+      for (var key2 in field[key]) {
+        for (let index = 0; index < field[key][key2].length; index++) {
+          if (jsonArray[i][field[key][key2][index]] != undefined) {
+            dynamics[y++] = jsonArray[i][field[key][key2][index]];
+          }
         }
       }
-      for (let index = 0; index < field.participant.course_group.length; index++) {
-        if (jsonArray[i][field.participant.course_group[index]] != undefined) {
-          courseG = jsonArray[i][field.participant.course_group[index]];
-        }
-      }
-      for (let index = 0; index < field.participant.prof_name.length; index++) {
-        if (jsonArray[i][field.participant.prof_name[index]] != undefined) {
-          pname = jsonArray[i][field.participant.prof_name[index]];
-        }
-        // console.log(jsonArray[i][field.participant.seat[index]])
-      }
-      for (let index = 0; index < field.participant.location.length; index++) {
-        if (jsonArray[i][field.participant.location[index]] != undefined) {
-          vloc = jsonArray[i][field.participant.location[index]];
-        }
-      }
-
-      for (let index = 0; index < field.participant.std_family_name.length; index++) {
-        if (jsonArray[i][field.participant.std_family_name[index]] != undefined) {
-          fname = jsonArray[i][field.participant.std_family_name[index]];
-        }
-      }
-      for (let index = 0; index < field.participant.std_number.length; index++) {
-        if (jsonArray[i][field.participant.std_number[index]] != undefined) {
-          stdno = jsonArray[i][field.participant.std_number[index]];
-        }
-      }
-      for (let index = 0; index < field.participant.prof_family_name.length; index++) {
-        if (jsonArray[i][field.participant.prof_family_name[index]] != undefined) {
-          profname = jsonArray[i][field.participant.prof_family_name[index]];
-        }
-      }
-      for (let index = 0; index < field.participant.seat.length; index++) {
-        if (jsonArray[i][field.participant.seat[index]] != undefined) {
-          seatno = jsonArray[i][field.participant.seat[index]];
-        }
-      }
-    }
   else if (key === "date") {
     for (let index = 0; index < field.date.length; index++) {
       if (jsonArray[i][field.date[index]] != undefined) {
@@ -169,20 +132,14 @@ function content(i, jsonArray, field) {
   } else {
     for (let index = 0; index < field[key].length; index++) {
       if (jsonArray[i][field[key][index]] != undefined) {
-        vsem[x++] = jsonArray[i][field[key][index]];
+        statics[x++] = jsonArray[i][field[key][index]];
       }
     }
   }
+
   return {
-    fname,
-    stdno,
-    profname,
-    seatno,
     dateform,
-    stdname,
-    courseG,
-    pname,
-    vsem,
-    vloc
+    dynamics,
+    statics
   }
 }
